6edc7cdffe050e2768d18c25e8b7631d714018c1
Adam Madej <34507800+arahmitz@users.noreply.github.com>
2026-01-06 16:29:00 +0100

    Freeze Transforms
    Added freeze_transforms.py function to tools
    Added freeze_transforms.png icon for in-Maya usage

---
+++ mtoolbox/tools/freeze_transforms.py
@@ -0,0 +1,15 @@
+import maya.cmds as cmds
+from mtoolbox.utils.selection import get_selection
+
+
+def freeze_transforms():
+    """
+    Freezes transformation of selected object
+    Using MEL: makeIdentity -apply true -t 1 -r 1 -s 1 -n 0 -pn 1
+    """
+
+    selection = get_selection("Freeze Transforms")
+    if not selection:
+        return
+    
+    cmds.makeIdentity(a=True, t=True, r=True, s=True, pn=True)
