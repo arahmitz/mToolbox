3a31900bc73eba56783519e70984ec1a35cddbca
Adam Madej <34507800+arahmitz@users.noreply.github.com>
2026-01-09 20:26:21 +0100

    Create Joints
    Added a tool that creates a joint chain at origin with clean JointOrients using parameters like `base_name` , `joint_count`, `spacing` in preparation for UI creation.

--- mtoolbox/tools/create_joints.py
+++ mtoolbox/tools/create_joints.py
@@ -1,21 +1,27 @@
 import maya.cmds as cmds
-from mtoolbox.utils.selection import get_selection
 
 def create_joints():
     """
-    Creates a chain with zeroed JO of user selected length and name. Selects the first joint at the end.
-     
-    If an object is selected, chain will be snapped to that object, otherwise it'll start at world origin.
+    Creates a chain with zero-ed JO of user selected length and name. Selects the first joint at the end.
     """
     spacing = 5
     joint_count = 3
     base_name = 'joint'
+    add_affix = True
+    root_joint = []
 
+    # Create parametered joint chain
     for joint in range(joint_count):
-        if joint == joint_count-1:
-            joint_name = base_name + '_' +  str(joint) + '_end'
-        else:
-            joint_name = base_name + '_' +  str(joint)
-        cmds.joint(name=joint_name, p=(joint * spacing, 0, 0))
 
-create_joints()
+        if add_affix and joint == joint_count-1:
+            joint_name = base_name + str(joint+1) + '_end'
+        else:
+            joint_name = base_name + str(joint+1)
+        
+        if joint == 0:
+            root_joint = joint_name
+
+        cmds.joint(name=joint_name, p=(joint * spacing, 0, 0))
+    
+    # Select root at the end
+    cmds.select(root_joint)
