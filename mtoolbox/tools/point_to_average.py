61a6c842574995c7cb988ae864f3c0d208654dc6
Adam Madej <34507800+arahmitz@users.noreply.github.com>
2026-01-14 17:28:42 +0100

    Point To Average
    Added a tool that adds point Constraint between objects and snaps the last object to them only in T.
    Added a proper icon

---
+++ mtoolbox/tools/point_to_average.py
@@ -0,0 +1,21 @@
+import maya.cmds as cmds
+from mtoolbox.utils.selection import get_selection
+
+def point_to_average():
+    """
+    Points (T) the last selected object to the average translation (T) of all other selected objects
+
+    When selected:
+    - Last object is being driven
+    - All others are the points for the average
+    """
+    selection = get_selection("Point To Average")
+
+    if selection:
+        if len(selection) < 2:
+            cmds.warning("Point tool needs at least 2 selected objects. Point.")
+            return
+        else:
+                # selection[-1] is the driven object
+                constraint = (cmds.pointConstraint(selection[:-1], selection[-1], mo=False))
+                cmds.delete(constraint) 
