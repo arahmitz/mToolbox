a7f933f64a951ce30a65a4734def056dd94e8bdd
Adam Madej <34507800+arahmitz@users.noreply.github.com>
2026-01-15 21:16:11 +0100

    Make ROM update
    Changed docstring for  Make ROM

--- mtoolbox/tools/make_rom.py
+++ mtoolbox/tools/make_rom.py
@@ -4,19 +4,29 @@ from mtoolbox.utils.selection import get_selection
 
 def make_rom():
     """
-    Sets keyframes every 10 frames on the selected objects starting from frame 0 up to
-    the current timeline end, inclusive. Intended for Range Of Movement testing.
+    Sets keyframes every 10 frames on the selected objects,
+    starting from the current timeline start up to the timeline end.
+
+    - Aligns keys to interval
+    - Preserves existing keys on the start frame
     """
-    start_time = int(cmds.playbackOptions(q=True, min=True))
-    end_time = int(cmds.playbackOptions(q=True, max=True)) # grabs end of the selected timeline
 
     interval = 10
+    start_time = int(cmds.playbackOptions(q=True, min=True)) # grabs the start of the selected timeline
+    end_time = int(cmds.playbackOptions(q=True, max=True)) # grabs end of the selected timeline
 
+    # align start time for next interval
     first_key = int(math.ceil(start_time/interval) * interval)
+
+    # save last keyframe w/o overwriting previous keys
+    if start_time < first_key:
+        cmds.currentTime(start_time)
+        cmds.setKeyframe(selection)
     
     selection = get_selection("Make ROM")
     if selection:
         # has to end at end time + interval to include endtime
-        for keyframe in range(start_time, end_time + interval, interval):
+        for keyframe in range(first_key, end_time + interval, interval):
             cmds.currentTime(keyframe)
             cmds.setKeyframe(selection)
+            
