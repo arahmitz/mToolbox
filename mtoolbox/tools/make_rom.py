4b1b11f559bad5e52792c8260d5dfa3de08bd287
Adam Madej <34507800+arahmitz@users.noreply.github.com>
2026-01-15 14:44:43 +0100

    Updated MakeROM to use ceil for new start times

--- mtoolbox/tools/make_rom.py
+++ mtoolbox/tools/make_rom.py
@@ -1,4 +1,5 @@
 import maya.cmds as cmds
+import math
 from mtoolbox.utils.selection import get_selection
 
 def make_rom():
@@ -6,10 +7,12 @@ def make_rom():
     Sets keyframes every 10 frames on the selected objects starting from frame 0 up to
     the current timeline end, inclusive. Intended for Range Of Movement testing.
     """
-    start_time = 0 # ROM always start at 0
+    start_time = int(cmds.playbackOptions(q=True, min=True))
     end_time = int(cmds.playbackOptions(q=True, max=True)) # grabs end of the selected timeline
 
     interval = 10
+
+    first_key = int(math.ceil(start_time/interval) * interval)
     
     selection = get_selection("Make ROM")
     if selection:
